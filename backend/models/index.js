e7503d2dec602390718f8f98f365277e5c12b2d4
Hugo Ganet <ganethugo@gmail.com>
2023-10-25 18:10:47 +0100

    validate Sequelize connection with mghwebapp db

--- backend/models/index.js
+++ backend/models/index.js
@@ -5,7 +5,7 @@ const process = require('process');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
-const db = {};
+console.log('config', config);const db = {};
 
 let sequelize;
 if (config.use_env_variable) {
