c4abca2df195e6547a33dbd538d4a3a9859837ae
Hugo Ganet <ganethugo@gmail.com>
2023-10-25 17:56:34 +0100

    separate front and back end.
    Seperate in two different folder and reinstall dependencies in appropriate folder

---
+++ backend/server/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const db = require('./models');
+
+// Test DB connection
+db.sequelize.authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(error => {
+    console.error('Unable to connect to the database:', error);
+  });
+  
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
