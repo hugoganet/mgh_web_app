4e3584bf1a49857a779b9060e3216dacb28d2af7
Hugo Ganet <ganethugo@gmail.com>
2023-11-03 15:12:41 +0000

    create all tests for productCategoryRank

---
+++ backend/tests/unit/models/productCategoryRank.test.js
@@ -0,0 +1,44 @@
+const { initializeDatabase, closeDatabase, db } = require('../../jest.setup'); 
+
+describe(`ProductCategoryRank Model Tests`, () => {
+    beforeAll(async () => {
+        await initializeDatabase();
+    });     
+
+    afterAll(async () => {
+        await closeDatabase();
+    });
+
+    // Basic tests for creating, updating, deleting, and retrieving data
+    test('Select a valid ProductCategoryRank', async () => {
+        const expectedVatRate = 1.0;
+        const productCategoryRank = await db.ProductCategoryRank.findByPk(1);
+        expect(parseFloat(productCategoryRank.rankingThresholdPercentage)).toBe(expectedVatRate);
+    });
+
+    
+    // Association tests
+    test('ProductCategoryRank belongsTo ProductCategory', async () => {
+        const productCategoryRank = await db.ProductCategoryRank.findAll({ 
+            where: { 
+                productCategoryId: 1,
+                countryCode: 'FR'
+            }});
+        expect(productCategoryRank.length).toBe(18);
+    });
+
+
+    test('ProductCategoryRank hasMany Asin', async () => {
+        const expectedProducCategoryRankId = 10;
+        const asins = await db.Asin.findAll({ where: { productCategoryRankId: expectedProducCategoryRankId } });
+        asins.forEach(asin => {
+          expect(asin.productCategoryRankId).toBe(expectedProducCategoryRankId);
+        });
+    });
+
+});
+
+
+
+
+
