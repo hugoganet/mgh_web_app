7b61ab02824b9632651456eacce06388e484d0e0
Hugo Ganet <ganethugo@gmail.com>
2023-11-06 18:08:17 +0000

    create all test for Asin model

--- backend/tests/unit/models/asin.test.js
+++ backend/tests/unit/models/asin.test.js
@@ -91,67 +91,147 @@ describe(`Asin Model Tests`, () => {
     expect(deletedAsin).toBeNull();
   });
 
-  // // ASSOCIATION TESTS
+  // ASSOCIATION TESTS
 
-  // // Asin belongsTo ProductCategory
-  // test('Asin belongs to ProductCategory', async () => {
-  //     createdAsin = await db.Asin.create({ /* ... */, productCategoryId: createdProductCategory.id });
-  //     const associatedCategory = await createdAsin.getProductCategory();
+  // Asin belongsTo ProductCategory
+  test('Fail to create an Asin with invalid ProductCategory', async () => {
+    expect.assertions(1); // This ensures that one assertion is called. Otherwise a fulfilled promise would not fail the test.
+    try {
+      await db.Asin.create({
+        asin: 'B01N5IB20Q',
+        countryCode: 'FR',
+        productCategoryId: 999, // This is an invalid productCategoryId
+        productCategoryRankId: 1,
+        productTaxCategoryId: 1,
+        asinName: 'Example Product Name',
+        urlAmazon: 'http://example.com/product',
+        urlImage: 'http://example.com/product.jpg',
+        asinPotentialWarehousesQuantity: 10,
+        asinNumberOfActiveSku: 5,
+        asinAverageUnitSoldPerDay: 2.5,
+        isBatteryRequired: false,
+        isHazmat: false,
+      });
+    } catch (error) {
+      expect(error).toBeDefined();
+    }
+  });
 
-  //     expect(associatedCategory).not.toBeNull();
-  //     expect(associatedCategory.id).toBe(createdProductCategory.id);
-  // });
+  // Asin belongsTo ProductCategoryRank
+  test('Fail to create an Asin with invalid ProductCategoryRank', async () => {
+    expect.assertions(1); // This ensures that one assertion is called. Otherwise a fulfilled promise would not fail the test.
+    try {
+      await db.Asin.create({
+        asin: 'B01N5IB22Q',
+        countryCode: 'FR',
+        productCategoryId: 1,
+        productCategoryRankId: 9999, // This is an invalid productCategoryRankId
+        productTaxCategoryId: 1,
+        asinName: 'Example Product Name 2',
+        urlAmazon: 'http://example.com/product',
+        urlImage: 'http://example.com/product.jpg',
+        asinPotentialWarehousesQuantity: 10,
+        asinNumberOfActiveSku: 5,
+        asinAverageUnitSoldPerDay: 2.5,
+        isBatteryRequired: false,
+        isHazmat: false,
+      });
+    } catch (error) {
+      expect(error).toBeDefined();
+    }
+  });
 
-  // // Asin belongsTo ProductCategoryRank
-  // test('Asin belongs to ProductCategoryRank', async () => {
-  //     createdAsin = await db.Asin.create({ /* ... */, productCategoryRankId: createdProductCategoryRank.id });
-  //     const associatedCategoryRank = await createdAsin.getProductCategoryRank();
+  // Asin belongsTo ProductTaxCategory
+  test('Fail to create an Asin with invalid ProductTaxCategory', async () => {
+    expect.assertions(1); // This ensures that one assertion is called. Otherwise a fulfilled promise would not fail the test.
+    try {
+      await db.Asin.create({
+        asin: 'B01N5IB22Q',
+        countryCode: 'FR',
+        productCategoryId: 1,
+        productCategoryRankId: 1,
+        productTaxCategoryId: 9999,
+        asinName: 'Example Product Name 2',
+        urlAmazon: 'http://example.com/product',
+        urlImage: 'http://example.com/product.jpg',
+        asinPotentialWarehousesQuantity: 10,
+        asinNumberOfActiveSku: 5,
+        asinAverageUnitSoldPerDay: 2.5,
+        isBatteryRequired: false,
+        isHazmat: false,
+      });
+    } catch (error) {
+      expect(error).toBeDefined();
+    }
+  });
 
-  //     expect(associatedCategoryRank).not.toBeNull();
-  //     expect(associatedCategoryRank.id).toBe(createdProductCategoryRank.id);
-  // });
+  // Asin belongsTo Country
+  test('Fail to create an Asin with invalid Country', async () => {
+    expect.assertions(1); // This ensures that one assertion is called. Otherwise a fulfilled promise would not fail the test.
+    try {
+      await db.Asin.create({
+        asin: 'B01N5IB22Q',
+        countryCode: 'US', // This is an invalid countryCode
+        productCategoryId: 1,
+        productCategoryRankId: 1,
+        productTaxCategoryId: 1,
+        asinName: 'Example Product Name 2',
+        urlAmazon: 'http://example.com/product',
+        urlImage: 'http://example.com/product.jpg',
+        asinPotentialWarehousesQuantity: 10,
+        asinNumberOfActiveSku: 5,
+        asinAverageUnitSoldPerDay: 2.5,
+        isBatteryRequired: false,
+        isHazmat: false,
+      });
+    } catch (error) {
+      expect(error).toBeDefined();
+    }
+  });
 
-  // // Asin belongsTo ProductTaxCategory
-  // test('Asin belongs to ProductTaxCategory', async () => {
-  //     createdAsin = await db.Asin.create({ /* ... */, productTaxCategoryId: createdProductTaxCategory.id });
-  //     const associatedTaxCategory = await createdAsin.getProductTaxCategory();
+  // Asin belongsToMany Ean through EanInAsin
+  test('Return all ASINs where eanInAsinQuantity is greater than 1', async () => {
+    expect.assertions(1);
 
-  //     expect(associatedTaxCategory).not.toBeNull();
-  //     expect(associatedTaxCategory.id).toBe(createdProductTaxCategory.id);
-  // });
+    // Fetch ASINs with their associated Eans records where eanInAsinQuantity > 1
+    const asinsWithMultipleEans = await db.Asin.findAll({
+      include: [
+        {
+          model: db.Ean,
+          through: {
+            model: db.EanInAsin,
+            where: { eanInAsinQuantity: { [db.Sequelize.Op.gt]: 1 } },
+          },
+          required: true,
+        },
+      ],
+    });
+    // Assuming there is at least one ASIN with eanInAsinQuantity > 1 for the test to pass
+    expect(asinsWithMultipleEans.length).toBeGreaterThan(0);
+  });
 
-  // // Asin belongsTo Country
-  // test('Asin belongs to Country', async () => {
-  //     createdAsin = await db.Asin.create({ /* ... */, countryCode: createdCountry.countryCode });
-  //     const associatedCountry = await createdAsin.getCountry();
+  // Asin belongsToMany Sku through AsinSku
+  test('Should return all Skus for Asin "B00OHVDP5K"', async () => {
+    // Fetch the Asin by the ASIN value
+    const asinRecord = await db.Asin.findOne({ where: { asin: 'B00OHVDP5K' } });
 
-  //     expect(associatedCountry).not.toBeNull();
-  //     expect(associatedCountry.countryCode).toBe(createdCountry.countryCode);
-  // });
+    // If Asin is not found, there's an error in the setup
+    if (!asinRecord) {
+      throw new Error(
+        'Asin with the specified ASIN code does not exist in the test database',
+      );
+    }
 
-  // // Asin belongsToMany Ean through EanInAsin
-  // test('Asin belongsToMany Ean through EanInAsin', async () => {
-  //     createdEan = await db.Ean.create({ /* ... */ });
-  //     createdAsin = await db.Asin.create({ /* ... */ });
-  //     await createdAsin.addEan(createdEan);
+    // Fetch the associated Skus using the automatically generated Sequelize method
+    const skusForAsin = await asinRecord.getSkus();
 
-  //     const associatedEans = await createdAsin.getEans();
-
-  //     expect(associatedEans).not.toBeNull();
-  //     expect(associatedEans.length).toBeGreaterThan(0);
-  //     expect(associatedEans.some(e => e.id === createdEan.id)).toBe(true);
-  // });
-
-  // // Asin belongsToMany Sku through AsinSku
-  // test('Asin belongsToMany Sku through AsinSku', async () => {
-  //     createdSku = await db.Sku.create({ /* ... */ });
-  //     createdAsin = await db.Asin.create({ /* ... */ });
-  //     await createdAsin.addSku(createdSku);
-
-  //     const associatedSkus = await createdAsin.getSkus();
-
-  //     expect(associatedSkus).not.toBeNull();
-  //     expect(associatedSkus.length).toBeGreaterThan(0);
-  //     expect(associatedSkus.some(s => s.id === createdSku.id)).toBe(true);
-  // });
+    console.log(skusForAsin);
+    // Assertions
+    expect(skusForAsin).not.toBeNull();
+    expect(Array.isArray(skusForAsin)).toBe(true);
+    skusForAsin.forEach(sku => {
+      expect(sku).toHaveProperty('sku');
+      expect(sku).toHaveProperty('countryCode');
+    });
+  });
 });
