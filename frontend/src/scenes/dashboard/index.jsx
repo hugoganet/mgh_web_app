6ac201b6609ad8ed0a3ebb343fdb399e29627d3b
Hugo Ganet <ganethugo@gmail.com>
2023-12-02 16:36:52 +0100

    create Header component

--- frontend/src/scenes/dashboard/index.jsx
+++ frontend/src/scenes/dashboard/index.jsx
@@ -1,5 +1,15 @@
+/* eslint-disable no-unused-vars */
+import { Box } from '@mui/material';
+import Header from '../../components/Header';
+
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <Box m="20px">
+      <Box display="flex" justifyContent="space-between" alignItems="center">
+        <Header title="Dashboard" subtitle="Welcome to the Dashboard" />
+      </Box>
+    </Box>
+  );
 };
 
 export default Dashboard;
