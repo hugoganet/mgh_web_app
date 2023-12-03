71224701c37dfae74a38f1b866c8a325da26e92b
Hugo Ganet <ganethugo@gmail.com>
2023-12-03 12:28:30 +0100

    create PieChart component

---
+++ frontend/src/scenes/pie/index.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable no-unused-vars */
+import { Box } from '@mui/material';
+import Header from '../../components/Header';
+import PieChart from '../../components/PieChart';
+
+const Pie = () => {
+  return (
+    <Box m="20px">
+      <Header title="Pie Chart" subtitle="Simple Pie Chart" />
+      <Box height="75vh">
+        <PieChart />
+      </Box>
+    </Box>
+  );
+};
+
+export default Pie;
