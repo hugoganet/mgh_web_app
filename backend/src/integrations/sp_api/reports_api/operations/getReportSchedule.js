122a13c39cc4a81f49ccb0a8c63dbb7aca199a82
Hugo Ganet <ganethugo@gmail.com>
2024-01-11 10:13:11 +0100

    create getReportSchedule

---
+++ backend/src/integrations/sp_api/reports_api/operations/getReportSchedule.js
@@ -0,0 +1,41 @@
+const { spApiInstance } = require('../../connection/spApiConnector');
+
+/**
+ * @function getReportSchedule
+ * @description Returns report schedule details for the report schedule that you specify.
+ * @async
+ * @param {Object} config - config to apply when fetching reports.
+ * @return {Promise<Object>} - ReportSchedule - Detailed information about a report schedule.
+ */
+async function getReportSchedule(config) {
+  const { reportScheduleId, createLog } = config;
+  const queryParams = {
+    reportScheduleId,
+  };
+  const apiOperation = 'getReportSchedule';
+  const endpoint = `/reports/2021-06-30/schedules/${reportScheduleId}`;
+  const method = 'GET';
+
+  try {
+    const response = await spApiInstance.sendRequest(
+      method,
+      endpoint,
+      queryParams,
+      {},
+      createLog,
+      apiOperation,
+    );
+    console.log(response.data);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+}
+
+module.exports = { getReportSchedule };
+
+config = {
+  reportScheduleId: '50038019732',
+  createLog: true,
+};
+getReportSchedule(config);
