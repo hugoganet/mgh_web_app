2604fa24922f4e266457e6971b599c0dc7f949bf
Hugo Ganet <ganethugo@gmail.com>
2024-01-15 17:13:02 +0100

    add some parameters to ReceiveMessageCommand(receiveParams)

--- backend/src/integrations/sp_api/notifications_api/AWS_sqs_queue/receiveAndProcessNotifications.js
+++ backend/src/integrations/sp_api/notifications_api/AWS_sqs_queue/receiveAndProcessNotifications.js
@@ -3,8 +3,14 @@ const {
   ReceiveMessageCommand,
   DeleteMessageCommand,
 } = require('@aws-sdk/client-sqs');
-// const { defaultProvider } = require('@aws-sdk/credential-provider-node');
 require('dotenv').config({ path: 'backend/.env' });
+// const {
+//   getReportDocument,
+// } = require('../../reports_api/operations/getReportDocument.js');
+// const {
+//   fetchAndProcessInventoryReport,
+// } = require('../../reports_api/fba_inventory_report/fetchAndProcessInventoryReport.js');
+// const { getReportSchedules } = require('../../reports_api/operations/getReportSchedules.js');
 
 // Initialize SQS client with the AWS region and credentials
 const sqsClient = new SQSClient({
@@ -28,6 +34,8 @@ async function receiveAndProcessNotifications() {
     QueueUrl: queueURL,
     MaxNumberOfMessages: 10, // Adjust as needed
     WaitTimeSeconds: 20, // Long polling
+    VisibilityTimeout: 10, // Adjust as needed
+    MessageRetentionPeriod: 1209600, // 14 days
   };
 
   try {
@@ -38,21 +46,47 @@ async function receiveAndProcessNotifications() {
       for (const message of data.Messages) {
         const notification = JSON.parse(message.Body);
 
+        // console.log(notification);
+
         // Process the notification based on its type
         if (notification.notificationType === 'REPORT_PROCESSING_FINISHED') {
-          // Implement your logic for this notification type
-          console.log('Report Processing Finished Notification:', notification);
+          console.log('REPORT_PROCESSING_FINISHED');
+          console.log(notification);
+          console.log('-------------------------------------');
+
+          const reportDocumentId =
+            notification.payload.reportProcessingFinishedNotification
+              .reportDocumentId;
+          const reportType =
+            notification.payload.reportProcessingFinishedNotification
+              .reportType;
+          // const { documentUrl, compressionAlgorithm } = await getReportDocument(
+          //   reportDocumentId,
+          //   true,
+          //   reportType,
+          // );
+
+          // TODO Get countryKeys before calling fetchAndProcessInventoryReport
+          const countryKeys = ['sweden'];
+          // // Fetch CSV data and process into database
+          // await fetchAndProcessInventoryReport(
+          //   documentUrl,
+          //   compressionAlgorithm,
+          //   reportDocumentId,
+          //   countryKeys,
+          //   reportType,
+          // );
         }
 
         // Add logic for other notification types as needed
 
-        // Delete the processed message from the queue
-        const deleteParams = {
-          QueueUrl: queueURL,
-          ReceiptHandle: message.ReceiptHandle,
-        };
-        const deleteCommand = new DeleteMessageCommand(deleteParams);
-        await sqsClient.send(deleteCommand);
+        // TODO Delete the processed message from the queue
+        // const deleteParams = {
+        //   QueueUrl: queueURL,
+        //   ReceiptHandle: message.ReceiptHandle,
+        // };
+        // const deleteCommand = new DeleteMessageCommand(deleteParams);
+        // await sqsClient.send(deleteCommand);
       }
     }
   } catch (error) {
