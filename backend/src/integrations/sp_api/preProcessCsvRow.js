bf1b7c40adbaec333a8b1cfdfb43276f793d66dc
Hugo Ganet <ganethugo@gmail.com>
2024-02-06 08:49:38 +0100

    add contextual logging logic to preProcessCsvRow.js

--- backend/src/integrations/sp_api/preProcessCsvRow.js
+++ backend/src/integrations/sp_api/preProcessCsvRow.js
@@ -1,13 +1,19 @@
+const { logger } = require('../../logger');
+
 /**
  * Preprocesses a single row from the CSV file.
  * @param {Object} chunk - The original row data from the CSV file.
+ * @param {string} logContext - The context for the log message.
+ * @param {boolean} createLog - Whether to create a log of the process.
  * @return {Object} The processed chunk.
  */
-function preProcessCsvRow(chunk) {
+function preProcessCsvRow(chunk, logContext, createLog) {
   if (chunk['product-name']) {
     chunk['product-name'] = chunk['product-name'].replace(/"/g, '');
+    if (createLog) {
+      logger(`Processed product-name: ${chunk['product-name']}\n`, logContext);
+    }
   }
-
   return chunk;
 }
 
