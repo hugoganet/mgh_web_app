64e47391f651acb7112697d56d4d392beae0bef1
Hugo Ganet <ganethugo@gmail.com>
2024-04-02 08:56:09 +0200

    update server/index.js so it only take care of starting the server

--- backend/src/server/index.js
+++ backend/src/server/index.js
@@ -1,19 +1,7 @@
 require('dotenv').config({ path: 'backend/.env' });
 const app = require('../app');
 const PORT = process.env.PORT || 3001; // Use Heroku's PORT or 3001 if local
-const initializeDatabase = require('../database/initializeDatabase');
 
-const isTest = process.env.NODE_ENV === 'test';
-
-(async () => {
-  if (isTest) {
-    console.log('Initializing database...');
-    await initializeDatabase({ forceSync: true });
-  } else {
-    await initializeDatabase({ forceSync: false }); // Typically you wouldn't force sync in non-test environments
-  }
-
-  app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-  });
-})();
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
