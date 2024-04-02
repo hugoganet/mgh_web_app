75403c6e9d280973ee897bd57b4f38e347055638
Hugo Ganet <ganethugo@gmail.com>
2024-04-02 09:04:38 +0200

    update jest.setup.js file to take care of test db initialization on itself

--- backend/tests/jest.setup.js
+++ backend/tests/jest.setup.js
@@ -1,7 +1,5 @@
 jest.setTimeout(30000); // Higher timeout for database operations
-
 const db = require('../src/database/models/index');
-const initializeDatabase = require('../src/database/initializeDatabase');
 
 /**
  * Initialize the database for testing.
@@ -10,7 +8,61 @@ const initializeDatabase = require('../src/database/initializeDatabase');
  * @return {Promise<void>} Promise representing the completion of initialization
  */
 async function initializeTestDatabase() {
-  await initializeDatabase({ forceSync: true });
+  try {
+    // Define the order of model synchronization
+    const modelOrder = [
+      'Country',
+      'Brand',
+      'VatCategory',
+      'ProductCategory',
+      'VatRatePerCountry',
+      'ProductCategoryRank',
+      'ProductTaxCategory',
+      'Ean',
+      'Asin',
+      'Sku',
+      'AsinSku',
+      'EanInAsin',
+      'Warehouse',
+      'AmazonReferralFee',
+      'ProductAndAmzReferralFeeCategory',
+      'PriceGridFbaFee',
+      'PricingRule',
+      'Supplier',
+      'Donation',
+      'SupplierOrder',
+      'MinimumSellingPrice',
+      'FbaFee',
+      'Catalog',
+      'SupplierBrandCatalog',
+      'EanInSupplierOrder',
+      'EanInDonation',
+      'WarehouseStock',
+      'AfnInventoryDailyUpdate',
+      'SellingPriceHistory',
+      'FbaSaleProcessed',
+      'DailyAverageExchangeRate',
+      'AfnShipments',
+      'AfnRemovalOrders',
+      'AfnRemovalOrdersDetails',
+      'AfnRemovalShipmentsDetails',
+      'EanInAfnRemovalOrders',
+      'EanInAfnShipments',
+    ];
+
+    // Synchronize each model in order
+    for (const modelName of modelOrder) {
+      if (db[modelName] && db[modelName].sync) {
+        await db[modelName].sync({ force: true });
+      }
+    }
+
+    await runSeeding(db);
+    await createSqlViews(db);
+    console.log('Test database initialization complete.');
+  } catch (error) {
+    console.error('Error initializing test database:', error);
+  }
 }
 
 /**
