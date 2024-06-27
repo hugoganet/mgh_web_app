48734521e9413734be4f9e8e4feec0196cda925c
Hugo Ganet <ganethugo@gmail.com>
2024-06-27 15:35:21 +0200

    update keepa_data migration file to include new data points

--- backend/src/database/migrations/20240416161420-create-keepa_data.js
+++ backend/src/database/migrations/20240416161420-create-keepa_data.js
@@ -181,6 +181,222 @@ module.exports = {
       is_hazmat: {
         type: Sequelize.BOOLEAN,
       },
+      sales_ranking_current: {
+        type: Sequelize.INTEGER,
+      },
+      reviews_count_30_days_avg: {
+        type: Sequelize.INTEGER,
+      },
+      reviews_count_90_days_avg: {
+        type: Sequelize.INTEGER,
+      },
+      reviews_count_180_days_avg: {
+        type: Sequelize.INTEGER,
+      },
+      map_restriction: {
+        type: Sequelize.STRING,
+      },
+      lowest_fba_seller: {
+        type: Sequelize.STRING,
+      },
+      list_price_current: {
+        type: Sequelize.DECIMAL(10, 2),
+      },
+      list_price_30_days_avg: {
+        type: Sequelize.DECIMAL(10, 2),
+      },
+      list_price_90_days_avg: {
+        type: Sequelize.DECIMAL(10, 2),
+      },
+      list_price_180_days_avg: {
+        type: Sequelize.DECIMAL(10, 2),
+      },
+      list_price_30_days_drop_percent: {
+        type: Sequelize.DECIMAL(5, 2),
+      },
+      list_price_90_days_drop_percent: {
+        type: Sequelize.DECIMAL(5, 2),
+      },
+      list_price_lowest: {
+        type: Sequelize.DECIMAL(10, 2),
+      },
+      list_price_highest: {
+        type: Sequelize.DECIMAL(10, 2),
+      },
+      tracking_since: {
+        type: Sequelize.DATE,
+      },
+      listed_since: {
+        type: Sequelize.DATE,
+      },
+      categories_root: {
+        type: Sequelize.STRING,
+      },
+      upc: {
+        type: Sequelize.CHAR(12),
+      },
+      part_number: {
+        type: Sequelize.STRING,
+      },
+      variation_asins: {
+        type: Sequelize.STRING,
+      },
+      freq_bought_together: {
+        type: Sequelize.STRING,
+      },
+      manufacturer: {
+        type: Sequelize.STRING,
+      },
+      variation_attributes: {
+        type: Sequelize.STRING,
+      },
+      item_dimension_cm3: {
+        type: Sequelize.DECIMAL(10, 2),
+      },
+      item_length_cm: {
+        type: Sequelize.DECIMAL(10, 2),
+      },
+      item_width_cm: {
+        type: Sequelize.DECIMAL(10, 2),
+      },
+      item_height_cm: {
+        type: Sequelize.DECIMAL(10, 2),
+      },
+      description: {
+        type: Sequelize.TEXT,
+      },
+      feature_1: {
+        type: Sequelize.STRING,
+      },
+      feature_2: {
+        type: Sequelize.STRING,
+      },
+      feature_3: {
+        type: Sequelize.STRING,
+      },
+      feature_4: {
+        type: Sequelize.STRING,
+      },
+      feature_5: {
+        type: Sequelize.STRING,
+      },
+      new_offer_count_30_days_avg: {
+        type: Sequelize.INTEGER,
+      },
+      new_offer_count_180_days_avg: {
+        type: Sequelize.INTEGER,
+      },
+      contributors: {
+        type: Sequelize.STRING,
+      },
+      package_quantity: {
+        type: Sequelize.INTEGER,
+      },
+      feature_6: {
+        type: Sequelize.STRING,
+      },
+      feature_7: {
+        type: Sequelize.STRING,
+      },
+      feature_8: {
+        type: Sequelize.STRING,
+      },
+      feature_9: {
+        type: Sequelize.STRING,
+      },
+      feature_10: {
+        type: Sequelize.STRING,
+      },
+      referral_fee_buy_box_price: {
+        type: Sequelize.DECIMAL(10, 2),
+      },
+      image_count: {
+        type: Sequelize.INTEGER,
+      },
+      buy_box_percent_amazon_30_days: {
+        type: Sequelize.DECIMAL(5, 2),
+      },
+      buy_box_percent_amazon_90_days: {
+        type: Sequelize.DECIMAL(5, 2),
+      },
+      buy_box_percent_amazon_180_days: {
+        type: Sequelize.DECIMAL(5, 2),
+      },
+      buy_box_percent_amazon_365_days: {
+        type: Sequelize.DECIMAL(5, 2),
+      },
+      buy_box_percent_top_seller_30_days: {
+        type: Sequelize.DECIMAL(5, 2),
+      },
+      buy_box_percent_top_seller_90_days: {
+        type: Sequelize.DECIMAL(5, 2),
+      },
+      buy_box_percent_top_seller_180_days: {
+        type: Sequelize.DECIMAL(5, 2),
+      },
+      buy_box_percent_top_seller_365_days: {
+        type: Sequelize.DECIMAL(5, 2),
+      },
+      buy_box_winner_count_30_days: {
+        type: Sequelize.INTEGER,
+      },
+      buy_box_winner_count_90_days: {
+        type: Sequelize.INTEGER,
+      },
+      buy_box_winner_count_180_days: {
+        type: Sequelize.INTEGER,
+      },
+      buy_box_winner_count_365_days: {
+        type: Sequelize.INTEGER,
+      },
+      competitive_price_threshold: {
+        type: Sequelize.DECIMAL(10, 2),
+      },
+      suggested_lower_price: {
+        type: Sequelize.DECIMAL(10, 2),
+      },
+      buy_box_eligible_offer_counts_new_fba: {
+        type: Sequelize.INTEGER,
+      },
+      buy_box_eligible_offer_counts_new_fbm: {
+        type: Sequelize.INTEGER,
+      },
+      buy_box_eligible_offer_counts_used_fba: {
+        type: Sequelize.INTEGER,
+      },
+      buy_box_eligible_offer_counts_used_fbm: {
+        type: Sequelize.INTEGER,
+      },
+      buy_box_eligible_offer_counts_collectible_fba: {
+        type: Sequelize.INTEGER,
+      },
+      buy_box_eligible_offer_counts_collectible_fbm: {
+        type: Sequelize.INTEGER,
+      },
+      buy_box_eligible_offer_counts_refurbished_fba: {
+        type: Sequelize.INTEGER,
+      },
+      buy_box_eligible_offer_counts_refurbished_fbm: {
+        type: Sequelize.INTEGER,
+      },
+      list_price_1_day_drop_percent: {
+        type: Sequelize.DECIMAL(5, 2),
+      },
+      list_price_7_days_drop_percent: {
+        type: Sequelize.DECIMAL(5, 2),
+      },
+      imported_by_code: {
+        type: Sequelize.STRING,
+      },
+      variation_count: {
+        type: Sequelize.INTEGER,
+      },
+      type: {
+        type: Sequelize.STRING,
+      },
+      hazardous_materials: {
+        type: Sequelize.STRING,
+      },
     });
 
     // Create a unique index on asin and country_code after the table creation
